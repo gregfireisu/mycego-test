606d9c898e1212bcde71cbc7323047c773891480
Grigory <gregfireisu@gmail.com>
2024-03-20 11:39:31 +0300

    Update mycego-test.py

--- mycego-test.py
+++ mycego-test.py
@@ -4,7 +4,6 @@ import os
 def create_collage_from_folders(folder_list, output_file):
     images = []
     
-    # Получение списка изображений из указанных папок
     for folder_name in folder_list:
         folder_path = os.path.join(os.getcwd(), folder_name)
         for filename in os.listdir(folder_path):
@@ -13,13 +12,11 @@ def create_collage_from_folders(folder_list, output_file):
                 img = Image.open(image_path)
                 images.append(img)
 
-    # Инициализация параметров для создания коллажа
     max_images_in_row = 4
     outer_padding = 20
     inner_padding = 40
-    background_color = (255, 255, 255)  # белый
+    background_color = (255, 255, 255)
 
-    # Вычисление размеров коллажа
     max_width = max(img.width for img in images)
     max_height = max(img.height for img in images)
     collage_width = max_width * max_images_in_row + (max_images_in_row - 1) * inner_padding + 2 * outer_padding
@@ -31,7 +28,6 @@ def create_collage_from_folders(folder_list, output_file):
     x_offset = outer_padding
     y_offset = outer_padding
 
-    # Создание коллажа
     for img in images:
         collage.paste(img, (x_offset, y_offset))
         x_offset += max_width + inner_padding
@@ -41,10 +37,8 @@ def create_collage_from_folders(folder_list, output_file):
             x_offset = outer_padding
             y_offset += max_height + 2 * outer_padding
 
-    # Сохранение коллажа в формате TIFF
     collage.save(output_file)
 
-# Пример использования
 folder_list = ['1388_12_Наклейки 3-D_3']
 output_file = 'Result.tif'
 
